622ba36fcd9969b0e54abddc51c46b9a950803bc
JamesLiang <James@cxy.com>
2026-05-13 18:19:58 +0800

    修改测试文件

--- MyFunctions/MyFunctions.cs
+++ MyFunctions/MyFunctions.cs
@@ -1,16 +1,10 @@
 using ExcelDna.Integration;
 
-namespace MyFunctions
+public static class TestUDF
 {
-    public static class UDFs
+    [ExcelFunction(Description="Returns 123")]
+    public static double Test()
     {
-        [ExcelFunction(Description = "Add two numbers")]
-        public static double MyAdd(double a, double b) => a + b;
-
-        [ExcelFunction(Description = "Multiply two numbers")]
-        public static double MyMultiply(double a, double b) => a * b;
-
-        [ExcelFunction(Description = "Return a greeting string")]
-        public static string SayHello(string name) => $"Hello, {name}!";
+        return 123;
     }
 }
